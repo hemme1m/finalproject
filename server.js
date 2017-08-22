275f3e8a92d0b2d69b77289efef2f18c4b5444b1
hemme1m <markhemmert@gmail.com>
2017-08-22 11:39:31 -0400

    set up server file, added git gnore for node_modules, created package.json

--- server.js
+++ server.js
@@ -1,7 +1,28 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const bodyParser = require('body-parser');
+const pg = require('pg');
 
-var server = app.listen(3000, function() {
-	var port = server.address().port;
-	console.log('Example app listening at http://localhost:%s', port);
+const app = express();
+// Serve files from public folder. That's where all of our HTML, CSS and Angular JS are.
+app.use(express.static('public'));
+// This allows us to accept JSON bodies in POSTs and PUTs.
+app.use(bodyParser.json());
+
+// TODO Set up access to the database via a connection pool. You will then use
+// the pool for the tasks below.
+
+var pool = new pg.Pool({
+	user: "postgres",
+	password: "mark1",
+	host: "localhost",
+	port: 5432,
+	database: "postgres",
+	ssl: false
+});
+
+//pool queries here
+
+var port = process.env.PORT || 5000;
+app.listen(port, function () {
+  console.log('JSON Server is running on ' + port);
 });
